5d7f34eb81fa8f094d610d2a740c2c044977970c
Ojaswi <ojaswideep2020@gmail.com>
2025-03-11 23:22:54 +0530

    Initial commit

---
+++ movies/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.conf import settings
+import requests
+
+# Create your views here.
+def landingPage(request):
+    API_KEY = settings.TMDB_API_KEY
+    category = request.GET.get('category', 'popular')
+    search_query = request.GET.get("search","")
+    
+    print(f'API_KEY: {API_KEY}')  # Debug print statement to verify API key
+    base_url = "https://api.themoviedb.org/3/movie/"
+
+    if search_query: 
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={search_query}"
+    else:
+        url = f"{base_url}{category}?api_key={API_KEY}"
+
+    try:
+        response = requests.get(url)
+        print(f'Status Code: {response.status_code}')
+        response.raise_for_status()  # Raise an error for bad status codes
+        data = response.json().get('results', [])
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
+        data = []
+    
+    error_message = f"Oops!! Something went wrong!"
+    return render(request, 'movies/landing_page.html', {'movies': data, 'category': category, 'search_query': search_query, 'error_message': error_message})
