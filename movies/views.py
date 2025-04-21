d8e9a2c796151f3043755f718bc5523fa65926ed
Ojaswi <ojaswideep2020@gmail.com>
2025-04-21 12:24:39 +0530

    initial commit

--- movies/views.py
+++ movies/views.py
@@ -7,14 +7,16 @@ def landingPage(request):
     API_KEY = settings.TMDB_API_KEY
     category = request.GET.get('category', 'popular')
     search_query = request.GET.get("search","")
-    
+    page = int(request.GET.get('page', 1))
+    next_page = page + 1
+
     print(f'API_KEY: {API_KEY}')  # Debug print statement to verify API key
     base_url = "https://api.themoviedb.org/3/movie/"
 
     if search_query: 
-        url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={search_query}"
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={search_query}&page={page}"
     else:
-        url = f"{base_url}{category}?api_key={API_KEY}"
+        url = f"{base_url}{category}?api_key={API_KEY}&page={page}"
 
     try:
         response = requests.get(url)
@@ -26,4 +28,11 @@ def landingPage(request):
         data = []
     
     error_message = f"Oops!! Something went wrong!"
-    return render(request, 'movies/landing_page.html', {'movies': data, 'category': category, 'search_query': search_query, 'error_message': error_message})
+    return render(request, 'movies/landing_page.html', {
+        'movies': data,
+        'category': category,
+        'search_query': search_query,
+        'error_message': error_message,
+        'page': page,
+        'next_page': next_page
+    })
